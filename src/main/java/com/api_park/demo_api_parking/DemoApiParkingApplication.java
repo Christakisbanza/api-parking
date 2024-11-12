03ab152d6bfdd6a63e61ea1fcf9ec7b9568b85db
Christakis <banzachristakis@gmail.com>
2024-11-12 16:43:55 -0300

    config TimeZone

---
+++ src/main/java/com/api_park/demo_api_parking/DemoApiParkingApplication.java
@@ -0,0 +1,13 @@
+package com.api_park.demo_api_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_park/demo_api_parking/DemoApiParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_park.demo_api_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
