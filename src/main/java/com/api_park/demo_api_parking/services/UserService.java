75f6a110d00f2c7be1bef428d5f8033207dfdd73
Christakis <banzachristakis@gmail.com>
2024-11-12 21:26:08 -0300

    Create User

--- src/main/java/com/api_park/demo_api_parking/services/UserService.java
+++ src/main/java/com/api_park/demo_api_parking/services/UserService.java
@@ -1,12 +1,19 @@
 package com.api_park.demo_api_parking.services;
 
+import com.api_park.demo_api_parking.entity.User;
 import com.api_park.demo_api_parking.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user){
+        return userRepository.save(user);
+    }
 }
