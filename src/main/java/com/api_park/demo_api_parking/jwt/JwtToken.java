d7baf8c3901789f9478487e8971c8f8c17e0ff4a
Christakis <banzachristakis@gmail.com>
2024-11-16 18:27:17 -0300

    Class JwtToken Created

---
+++ src/main/java/com/api_park/demo_api_parking/jwt/JwtToken.java
@@ -0,0 +1,13 @@
+package com.api_park.demo_api_parking.jwt;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtToken {
+    private String token;
+
+}
