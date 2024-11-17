f887e11fbe3325a4a403aa1b2c8cb7e6febdf985
Christakis <banzachristakis@gmail.com>
2024-11-17 20:51:33 -0300

    Admin authorization

--- src/main/java/com/api_park/demo_api_parking/web/controller/UserController.java
+++ src/main/java/com/api_park/demo_api_parking/web/controller/UserController.java
@@ -16,6 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -80,6 +81,7 @@ public class UserController {
 
     )
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole(ADMIN)")
     public ResponseEntity<UserResponseDto> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(UserMapper.toUserDto(user));
